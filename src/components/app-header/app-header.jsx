cb7ad0351d388cce0c671d6e560ed49a7c15ae03
Sveta Haustova <sveta.suprun.hsi@yandex.ru>
2022-02-27 11:55:45 +0300

    ref: change css

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -5,29 +5,33 @@ import styles from './app-header.module.css';
 function AppHeader() {
     return (
         <header className={styles.header}>
-            <nav className={styles.header__container}>
+            <nav>
                 <ul className={styles.header__list}>
                     <li className={styles.header__item}>
                         <a className={styles.header__link} href="#top">
                             <BurgerIcon type="primary" />
-                            <p className="text text_type_main-default pl-2 m-0">Конструктор</p>
+                            <p className="text text_type_main-default ml-2 m-0">Конструктор</p>
                         </a>
                     </li>
                     <li className={styles.header__item}>
                         <a className={styles.header__link} href="#top">
                             <ListIcon type="secondary" />
-                            <p className="text text_type_main-default text_color_inactive pl-2 m-0">Лента заказов</p>
+                            <p className="text text_type_main-default text_color_inactive ml-2 m-0">Лента заказов</p>
+                        </a>
+                    </li>
+                    <li className={styles.header__logo}>
+                        <a className={styles.header__link} href="#top">
+                            <Logo />
+                        </a>
+                    </li>
+                    <li className={styles.header__item}>
+                        <a className={styles.header__link} href="#top">
+                            <ProfileIcon type="secondary" />
+                            <p className="text text_type_main-default text_color_inactive ml-2 m-0">Личный кабинет</p>
                         </a>
                     </li>
                 </ul>
             </nav>
-            <Logo />
-            <div className={styles.header__login}>
-                <a className={styles.header__link} href="#top">
-                    <ProfileIcon type="secondary" />
-                    <p className="text text_type_main-default text_color_inactive pl-2 m-0">Личный кабинет</p>
-                </a>
-            </div>
         </header>
     );
 }
